211fc82a4a9c2cff46353e219b4decbb7d6a2e38
Lucio Torre <lucio.torre@gmail.com>
2012-05-03 19:52:06 -0300

    validated algorithm R with kolmogorov-smirnov

--- uniform_test.go
+++ uniform_test.go
@@ -4,7 +4,7 @@ import (
     "testing"
     )
 
-func Test_Update(t *testing.T) {
+func Test_R_Update(t *testing.T) {
     s := NewUniformSampleR(10)
     if (s.Count != 0) {
         t.Fatalf("Uniform Sample count not zero. (got %d)", s.Count)
@@ -22,5 +22,29 @@ func Test_Update(t *testing.T) {
            t.Fatalf("Uniform Sample at %d not 1. (got %d)", i, s.Count)
         }
     }
+}
+
+
+/* Use Kolmogorov-Smirnov test to validate that the sample is random */
+
+func Test_R_Kolmogorov(t *testing.T) {
+    start := 0
+    end := 100000
+    cdf := func (x float64) float64 {
+            return (x - float64(start)) / float64(end - start)
+    }
+
+    s := NewUniformSampleR(10000)
+
+    for i := start; i < end; i++ {
+        s.Update(float64(i))
+    }
+
+    D, pvalue := KolmogorovTest(s.Sample, cdf)
+
+    if pvalue < 0.005 {
+        t.Errorf("KolmogorovTest(sample) == %f, D == %f", pvalue, D)
+    }
+
 }
 
